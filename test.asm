4e5021541f0385f66c3412bcfa5fcfffe392b790
ThuWangzw <wangzw834@163.com>
2018-10-19 14:39:05 +0800

    demo

---
+++ test.asm
@@ -0,0 +1,12 @@
+.386
+.model flat, stdcall
+option casemap : none
+include Irvine32.inc
+include test.inc
+includelib "g:\masm32\lib\Irvine32.lib"
+includelib "g:\masm32\lib\kernel32.lib"
+includelib "g:\masm32\lib\user32.lib"
+
+.data
+mword DWORD 44
+end
