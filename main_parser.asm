7b499f8fe68050567fd596703d192fa7fedc7102
NaXin <naxinlegend@outlook.com>
2018-10-24 00:34:39 +0800

    begin writing the parser process

--- main_parser.asm
+++ main_parser.asm
@@ -18,10 +18,11 @@ includelib shell32.lib
 
 include global.inc
 
+; ======================================= Parser Using Structure ==============================
 Instructionproto STRUCT
     operation_type DWORD ?
     operation_len  DWORD ?	
-    operation_str  BYTE 4 DUP (?)
+    operation_str  BYTE 8 DUP (?)
     operand1_type  DWORD ?
     operand1_name  BYTE 8 DUP (?)
 	operand1_len   DWORD ?
@@ -30,8 +31,17 @@ Instructionproto STRUCT
 	operand2_len	DWORD ?
 Instructionproto ENDS
 
+Labelproto STRUCT
+	strp BYTE 9 DUP (0)
+	off DWORD ?
+Labelproto ENDS
+
+
 .data
 ; ====================================== temp vars ==============================================
+
+InstructionTable Instructionproto 100 DUP (<>)
+LabelTable Labelproto 100 DUP (<>)
 
 my_name1 BYTE "dsa",0
 my_name2 BYTE "lo",0
@@ -59,6 +69,8 @@ showStr_string BYTE "%s,",0
 
 msg_argument BYTE "missing filename....",10,0
 msg_openfile BYTE "cannot open input file...",10,0
+msg_grammar_err BYTE "syntax error with line number: lalal", 10, 0
+msg_begin_asm BYTE "Assembling: %s",10,0
 
 ; ====================================== cmd relative ==========================================
 
@@ -73,6 +85,17 @@ FileLine BYTE 128 DUP(0)
 ; ====================================== program relative ======================================
 NewLine BYTE 13,10,0
 
+; ====================================== parser relative =======================================
+splitList DWORD 10 DUP(?)
+dataOffset DWORD 0
+whichSection DWORD 0 ; 1 for data, 2 for code
+
+; ====================================== enumerate ============================================
+
+enumerateDataType BYTE "BYTE",0,"DWORD",0,0
+enumerateBYTEType BYTE "BYTE",0,0
+enumerateDWORDType BYTE "DWORD",0,0
+enumerateInstruction BYTE "ADD",0,"SUB",0,"MOV",0,"AND",0,"OR",0,"XOR",0,"CMP",0,"PUSH",0,"POP",0,"NEG",0,"XCHG",0,"LOOP",0,"CALL",0,"JMP",0,0
 
 .code
 ; ====================================== utilities for the code ================================
@@ -108,6 +131,136 @@ str_copy PROC USES eax ecx esi edi,
 	rep movsb
 	ret
 str_copy ENDP
+
+; ---------------------------------------------------
+str_cmp PROC USES eax edx esi edi,
+	string1: PTR BYTE,
+	string2: PTR BYTE
+;
+; cmp two strings
+;----------------------------------------------------
+	mov esi, string1
+	mov edi, string2
+	str_cmp_L1:
+	mov al, [esi]
+	mov dl, [edi]
+	cmp al, 0
+	jne str_cmp_L2
+	cmp dl, 0
+	jne str_cmp_L2
+	jmp str_cmp_L3
+
+	str_cmp_L2:
+	inc esi
+	inc edi
+	cmp al,dl
+	je str_cmp_L1
+
+	str_cmp_L3:
+	ret
+str_cmp ENDP
+
+; -------------------------------------------------------
+is_space PROC, sour: BYTE
+;
+; judge if the input is a space or \t, return 1 if space, 0 if not, 2 if the input is 0
+; ------------------------------------------------------
+	cmp sour, 32
+	je is_space_true
+	cmp sour, 9
+	je is_space_true
+	cmp sour, 0
+	je is_space_zero
+
+	mov eax, 0
+	ret
+	is_space_true:
+	mov eax, 1
+	ret
+	is_space_zero:
+	mov eax, 2
+	ret
+is_space ENDP
+
+; -------------------------------------------------------
+split_string PROC USES ebx ecx edx esi
+;
+; split the FileLine string
+; store the result in the splitList, return the count(mul 4)
+; -------------------------------------------------------
+	LOCAL splitStatus: DWORD
+	mov ebx, 0 ;splitCount
+	mov splitStatus, 0
+	mov esi, OFFSET FileLine
+
+	split_string_OUTERLOOP:
+		mov al, [esi]
+		cmp al, 0; L[i] != 0
+		je split_string_END
+
+		cmp splitStatus, 1
+		je split_string_FindEnd
+		
+		split_string_FindBegin:
+		INVOKE is_space, [esi]
+		cmp eax, 0
+		je split_string_ChangeToEnd
+		inc esi
+		jmp split_string_OUTERLOOP
+		
+		split_string_ChangeToEnd:
+		mov splitList[ebx], esi
+		add ebx, 4
+		inc esi
+		mov splitStatus, 1
+		jmp split_string_OUTERLOOP
+
+		split_string_FindEnd:
+		INVOKE is_space, [esi]
+		cmp eax, 1
+		je split_string_ChangeToBegin
+		inc esi
+		jmp split_string_OUTERLOOP
+
+		split_string_ChangeToBegin:
+		mov al, 0
+		mov [esi], al
+		inc esi
+		mov splitStatus, 0
+		jmp split_string_OUTERLOOP
+
+	split_string_END:
+	mov eax, ebx
+	ret
+split_string ENDP
+
+;---------------------------------------------------------
+inEnumerate PROC USES ebx ecx edx esi,
+	target: PTR BYTE, enumerate: PTR BYTE
+; find in Enumerate, return 1 if in, 0 ifnot
+; --------------------------------------------------------
+	mov ebx, target
+	mov edx, enumerate
+	inEnumerate_LOOP:
+	INVOKE str_cmp, ebx, edx
+	je inEnumerate_res_one
+	mov ebx, target
+	INVOKE str_len, edx
+	add edx, eax
+	inc edx
+	mov al, [edx]
+	cmp al, 0
+	je inEnumerate_res_zero
+	jmp inEnumerate_LOOP
+
+	inEnumerate_res_one:
+	mov eax, 1
+	ret
+	inEnumerate_res_zero:
+	mov eax, 0
+	ret
+inEnumerate ENDP
+
 
 ; ========================================= show tables ===========================================
 
@@ -184,7 +337,16 @@ pushGlobalV PROC USES eax ebx edx,
 	mov ebx, TYPE GlobalvSymbolTable
 	mul ebx
 	lea edx, GlobalvSymbolTable[eax].n_name
+
+	cmp [strp], 0
+	je pushGlobalV_off
 	INVOKE str_copy, strp, edx
+	jmp pushGlobalV_Con1
+	pushGlobalV_off:
+	mov ebx, DWORD PTR strp
+	mov DWORD PTR [edx], ebx
+
+	pushGlobalV_Con1:
 	mov ebx, in_val
 	mov GlobalvSymbolTable[eax].n_value, ebx
 	inc GlobalVCount
@@ -192,11 +354,12 @@ pushGlobalV PROC USES eax ebx edx,
 pushGlobalV ENDP
 
 ;------------------------------------------
-pushStringTable PROC USES eax edx,
+pushStringTable PROC USES ebx edx,
 	strp:PTR BYTE
 ;
-; push a string into the table
+; push a string into the table, return the begining of a string
 ;------------------------------------------
+	mov ebx, DWORD PTR StringTable
 	mov edx, DWORD PTR StringTable
 	add edx, OFFSET StringTable
 	INVOKE str_len, strp
@@ -205,6 +368,7 @@ pushStringTable PROC USES eax edx,
 	mov edx, DWORD PTR StringTable
 	add edx, eax
 	mov DWORD PTR StringTable, edx
+	mov eax, ebx
 	ret
 pushStringTable ENDP
 
@@ -240,6 +404,8 @@ OpenCmdFile PROC USES eax ebx ecx edx esi
     add esi, 4
 	INVOKE WideCharToMultiByte, 0,0,[esi],-1,OFFSET argument,SIZEOF argument,0,0
 	
+	INVOKE crt_printf, OFFSET msg_begin_asm, ADDR argument
+
 	;try open the file and give the handle
 	INVOKE CreateFile, OFFSET argument, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0
 	cmp eax, INVALID_HANDLE_VALUE
@@ -260,12 +426,13 @@ OpenCmdFile PROC USES eax ebx ecx edx esi
 OpenCmdFile ENDP
 
 ;----------------------------------------------------
-ReadLine PROC USES eax ebx ecx edx esi
+ReadLine PROC USES ebx ecx edx esi
 ;
-; read a line from file, put it to FileLine
+; read a line from file, put it to FileLine, return the length
 ;----------------------------------------------------
 	LOCAL readCount: DWORD
 	mov esi, OFFSET FileLine
+	mov ebx, 0
 	ReadLine_LOOP:
 	INVOKE ReadFile, FileHandle, esi, 1, ADDR readCount, 0
 
@@ -281,43 +448,158 @@ ReadLine PROC USES eax ebx ecx edx esi
 	; je ReadLine_END
 
 	inc esi
+	inc ebx
 	jmp ReadLine_LOOP
 
 	ReadLine_END:
 	mov al, 0
 	mov [esi], al
+	mov eax, ebx
 	ret
 ReadLine ENDP
 
-; ======================================== MAIN PROC ==============================================
+; ========================================= Parser PROC ============================================
 
-main PROC
+; -------------------------------------------------
+ParseDataDec PROC USES eax ebx ecx edx esi
+;
+; the FileLine is a data declaration, parse this declaration
+; ---------------------------------------------------
+	LOCAL NameStr:DWORD
+	; data declaration as some_name BYTE ?
+	INVOKE split_string
+
+	;see if the size is enough
+	cmp eax, 12
+	jl ParseDataError
+
+
+	mov ebx, splitList[0] ;value name
+	mov edx, splitList[4] ;value type
+	mov ecx, splitList[8] ;init value (as a string)
+
+	; ---------------------------- Name
+	INVOKE str_len, ebx
+	cmp eax, 8
+	jl ParseDataDec_pushGLO
+
+	; push to string table first
+	INVOKE pushStringTable, ebx
+	mov WORD PTR [NameStr+2], ax
+	mov WORD PTR [NameStr], 0
+	INVOKE pushGlobalV, ADDR NameStr, dataOffset
+	jmp ParseDataDec_Con1
+
+	ParseDataDec_pushGLO: ; directly push
+	INVOKE pushGlobalV, ebx, dataOffset
+
+	ParseDataDec_Con1:
+	; ---------------------------- type
+	INVOKE inEnumerate, edx, OFFSET enumerateBYTEType
+	cmp eax, 1
+	je ParseDataDec_BYTE
+
+	INVOKE inEnumerate, edx, OFFSET enumerateDWORDType
+	cmp eax, 1
+	je ParseDataDec_DWORD
+
+	jmp ParseDataError
+
+	ParseDataDec_BYTE:
+	add dataOffset, 1
+	jmp ParseDataDec_Con2
+	ParseDataDec_DWORD:
+	add dataOffset, 8
+	jmp ParseDataDec_Con2
+
+	ParseDataDec_Con2:
+
+	;--------------------------------- Init
+	
+	ret
+	ParseDataError:
+	INVOKE StdOut, OFFSET msg_grammar_err
+	INVOKE ExitProcess, 0
+ParseDataDec ENDP
+
+;-------------------------------------------------------
+ParseTextStr PROC USES eax ebx ecx edx esi
+;
+; the FileLine is a code line, parser this line
+;-------------------------------------------------------
+	ret
+ParseTextStr ENDP
+
+; ======================================== MAIN PROC ===============================================
+
+;-------------------------------------------------
+MainParser PROC USES eax ebx ecx edx esi edi
+;
+; main parser proc
+;-------------------------------------------------
+
 	INVOKE InitParser
 	INVOKE OpenCmdFile
-	
-	INVOKE ReadLine
-	INVOKE StdOut, OFFSET FileLine
-	mov ebx, eax
-	INVOKE StdOut, OFFSET NewLine
-	INVOKE crt_printf, OFFSET util_d, ebx
-	
-	INVOKE ReadLine
-	INVOKE StdOut, OFFSET FileLine
-	mov ebx, eax
-	INVOKE StdOut, OFFSET NewLine
-	INVOKE crt_printf, OFFSET util_d, ebx
 
+	MainParser_Parsering:
 	INVOKE ReadLine
-	INVOKE StdOut, OFFSET FileLine
-	mov ebx, eax
-	INVOKE StdOut, OFFSET NewLine
-	INVOKE crt_printf, OFFSET util_d, ebx
 
-	INVOKE ReadLine
-	INVOKE StdOut, OFFSET FileLine
-	mov ebx, eax
-	INVOKE StdOut, OFFSET NewLine
-	INVOKE crt_printf, OFFSET util_d, ebx
+	; judge if end
+	cmp BYTE PTR [FileLine], 0
+	je MainParser_FileEnd
+
+	; judge if data or code
+	cmp whichSection, 1
+	je MainParser_Data
+	cmp whichSection, 2
+	je MainParser_Code
+
+	; both not, start of the file, declaration
+	--------------------- Not implemented! -----------------------------------
+	jmp MainParser_Parsering
+
+	; data section
+	MainParser_Data:
+	INVOKE ParseDataDec
+	jmp MainParser_Parsering
+
+	; code section
+	MainParser_Code:
+	INVOKE ParseTextStr
+	jmp MainParser_Parsering
+
+	MainParser_FileEnd:
+	ret
+MainParser ENDP
+
+main PROC
+
+	;INVOKE ReadLine
+	;INVOKE ParseDataDec
+	
+	;INVOKE ReadLine
+	;INVOKE StdOut, OFFSET FileLine
+	;mov ebx, eax
+	;INVOKE StdOut, OFFSET NewLine
+	;INVOKE crt_printf, OFFSET util_d, ebx
+	
+	;INVOKE ReadLine
+	;INVOKE StdOut, OFFSET FileLine
+	;mov ebx, eax
+	;INVOKE StdOut, OFFSET NewLine
+	;INVOKE crt_printf, OFFSET util_d, ebx
+
+	;INVOKE ReadLine
+	;INVOKE StdOut, OFFSET FileLine
+	;mov ebx, eax
+	;INVOKE StdOut, OFFSET NewLine
+	;INVOKE crt_printf, OFFSET util_d, ebx
+
+	;INVOKE ReadLine
+	;INVOKE StdOut, OFFSET FileLine
+	;mov ebx, eax
+	;INVOKE StdOut, OFFSET NewLine
+	;INVOKE crt_printf, OFFSET util_d, ebx
     ;invoke LocalFree, dword ptr [szArglist] ; Free the memory occupied by CommandLineToArgvW
 	; mov eax, 5
 	; mov GlobalvSymbolTable[0].n_value, eax
